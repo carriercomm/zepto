045263276dc0769d526fa0f94f7f859f1f523ab0
Ramin B <i8ramin@gmail.com>
2012-03-28 18:03:05 -0400

    do not copy over undefined values when calling extend

--- src/zepto.js
+++ src/zepto.js
@@ -96,7 +96,10 @@ var Zepto = (function() {
 
   $.extend = function(target){
     slice.call(arguments, 1).forEach(function(source) {
-      for (key in source) target[key] = source[key];
+      // don't bring in undefined values
+      for (key in source)
+        if (source[key] !== undefined)
+          target[key] = source[key];
     })
     return target;
   }
